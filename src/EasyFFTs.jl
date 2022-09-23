712b94866424e015d432ff10b08b6f3e5891c6dc
KronosTheLate <densbal@live.no>
2022-09-23 15:46:25 +0200

    first commit

--- src/EasyFFTs.jl
+++ src/EasyFFTs.jl
@@ -1,5 +1,11 @@
+# pkg"activate /home/dennishb/GeekyStuff/Julia/Packages/EasyFFTs"
 module EasyFFTs
 
-# Write your package code here.
+import FFTW
 
+function fft(s::AbstractArray{<:Real})
+    return FFTW.fft(s)
+end
+
+fft(sin.(1:10))
 end
