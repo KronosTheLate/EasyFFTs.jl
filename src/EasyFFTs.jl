ec26d37dc4cb2e93f812993731f704d57c62f84c
KronosTheLate <densbal@live.no>
2022-09-30 13:09:08 +0200

    Reference FFTW a couple of places

--- src/EasyFFTs.jl
+++ src/EasyFFTs.jl
@@ -6,65 +6,72 @@ import FFTW
 """
 Doctstring goes here
 """
-function fft end
-export fft
+function easyfft end
+export easyfft
 
-function fft(s::AbstractArray; scalebylength=true)
+function easyfft(s::AbstractArray; scalebylength=true)
+    response = FFTW.fft(s)
     if scalebylength
-        return FFTW.fft(s) ./ length(s)
-    else
-        return FFTW.fft(s)
+        response ./= length(response)
     end
+    return response
 end
-function fft(s::AbstractArray, fs::Real; scalebylength=true)
-    N = length(s)
+function easyfft(s::AbstractArray, fs::Real; scalebylength=true)
+    response = FFTW.fft(s)
     if scalebylength
-        return (freq=FFTW.fftfreq(N, fs), resp=FFTW.fft(s) ./ N)
-    else
-        return (freq=FFTW.fftfreq(N, fs), resp=FFTW.fft(s))
+        response ./= length(response)
     end
-end
-
-function fft(s::AbstractArray{<:Real}; scalebylength=true)
-    if scalebylength
-        return FFTW.rfft(s) ./ length(s)
-    else
-        return FFTW.rfft(s)
-    end
-end
-function fft(s::AbstractArray{<:Real}, fs::Real; scalebylength=true)
-    N = length(s)
-    if scalebylength
-        return (freqs=FFTW.rfftfreq(N, fs), resp=FFTW.rfft(s) ./ N)
-    else
-        return (freqs=FFTW.rfftfreq(N, fs), resp=FFTW.rfft(s))
-    end
+    return (freqs=FFTW.fftfreq(length(s), fs), resp=response)
 end
 
+function easyfft(s::AbstractArray{<:Real}; scalebylength=true)
+    response = FFTW.rfft(s)
+    response = vcat(response[1], response[2:end])
+    if scalebylength
+        response ./= length(response)
+    end
+    return response
+end
+function easyfft(s::AbstractArray{<:Real}, fs::Real; scalebylength=true)
+    response = FFTW.rfft(s)
+    response = vcat(response[1], response[2:end])
+    if scalebylength
+        response ./= length(response)
+    end
+    return (freqs=FFTW.rfftfreq(length(s), fs), resp=response)
+end
 
-fft(sin.(1:10))
-fft(sin.(1:10), 1)
+end #module
+
+using EasyFFTs
+
+easyfft(sin.(1:10))
+easyfft(sin.(1:10), 1)
 
 ##? Internal testing
 using WGLMakie
-let
-    fs = 100
+##! Make easyfft function
+##! Make _easyfft_withfreq,
+##! and _easyfft_withoutfreq. Or internal helper functions?
+##! kwarg f, default to identity, applies to response.
+let  #! Why is amp off by faactor of 2?
+    fs = 1000
     duration = 1
     ts = range(0, duration, step=1 / fs)
 
-    f = 50
+    f = 5
     A = 2
-    s = @. A * sin(f * 2π * ts)
-    fig, ax, _ = stem(ts, s, axis=(label="Signal",))
+    s1 = @. A * sin(f * 2π * ts)
+    s2 = @. 1 * sin(2f * 2π * ts)
+    s = s1 .+ s2
+    fig, ax, _ = stem(ts, s, axis=(title="Signal",))
 
-    s_fft = fft(s, fs)
-    stem(fig[2, 1], s_fft.freqs, s_fft.resp .|> abs, axis=(label="FFT of signal",))
+    s_fft = easyfft(s, fs)
+    stem(fig[2, 1], s_fft.freqs, s_fft.resp .|> abs, axis=(title="FFT of signal",))
 
-    resp, freqs = fft(s, fs)
-    stem(fig[3, 1], freqs, resp .|> abs, axis=(label="FFT of signal",))
+    freqs, resp = easyfft(s, fs)
+    stem(fig[3, 1], freqs, resp .|> abs, axis=(title="FFT of signal",))
 
     fig
-
-end
 
 end
