93cbf57dbb40835880e89bcfec95f9816f49dfe8
KronosTheLate <densbal@live.no>
2022-10-03 13:06:59 +0200

    Document easyfft

--- src/EasyFFTs.jl
+++ src/EasyFFTs.jl
@@ -4,44 +4,84 @@ module EasyFFTs
 import FFTW
 
 """
-Doctstring goes here
+    easyfft(s)
+    easyfft(s, fs)
+
+
+# Keyword arguments
+- `scalebylength::Bool`: determines if the response is scaled by its length. Defaults to `true`.
+- `f::Function`: an optional function to apply elementwise to the response.
+
+Compute the Discrete Fourier Transform (DFT) of the 
+input vector `s`, scaling by `length(s)` by default. 
+This function uses FFTW.rfft if `s` has real elements, 
+and FFTW.fft otherwise.
+
+If a sampling frequency `fs` is supplied, the output becomes 
+a NamedTuple with keys `freq` and `resp`, containing the 
+freqiencues and response respectivly.
+
+This function offers four main benefits to using the FFTW functions directly:
+- The output is scaled by default, making the absolute value of the response 
+correspond directly to the amplitude of the sinusoids that make up the signal.
+- Simple and short syntax for getting the associated frequencies
+- Freqiencies and response are sorted by increasing frequency
+- `rfft` is automatically called for real element vectors, avoiding 
+the common newbie mistake of always using `fft`. Benefits are faster computation 
+and automtically discarding half of the symmetric spectrum. If you want both 
+sides of the spectrum, see [`easymirror`](@ref).
 """
 function easyfft end
 export easyfft
 
-function easyfft(s::AbstractVector; scalebylength=true)
+function easyfft(s::AbstractVector; scalebylength=true, f::Function=identity)
     response = FFTW.fft(s)
     if scalebylength
         response ./= length(response)
     end
-    return FFTW.fftshift(response)
+    response = FFTW.fftshift(response)
+    if f == identity
+        return response
+    else
+        return f.(response)
+    end
 end
-function easyfft(s::AbstractVector, fs::Real; scalebylength=true)
+function easyfft(s::AbstractVector, fs::Real; scalebylength=true, f::Function=identity)
     response = FFTW.fft(s)
     if scalebylength
         response ./= length(response)
+    end
+    if f == identity
+        return response
+    else
+        return f.(response)
     end
     return (freqs=FFTW.fftshift(FFTW.fftfreq(length(s), fs)), resp=FFTW.fftshift(response))
 end
 
-function easyfft(s::AbstractVector{<:Real}; scalebylength=true)
+function easyfft(s::AbstractVector{<:Real}; scalebylength=true, f::Function=identity)
     response = FFTW.rfft(s)
-    response = vcat(response[1], response[2:end])
     if scalebylength
         response ./= length(response)
     end
-    return response
+    if f == identity
+        return response
+    else
+        return f.(response)
+    end
 end
-function easyfft(s::AbstractVector{<:Real}, fs::Real; scalebylength=true)
+function easyfft(s::AbstractVector{<:Real}, fs::Real; scalebylength=true, f::Function=identity)
     response = FFTW.rfft(s)
-    response = vcat(response[1], response[2:end])
     if scalebylength
         response ./= length(response)
+    end
+    if f == identity
+        return response
+    else
+        return f.(response)
     end
     return (freqs=FFTW.rfftfreq(length(s), fs), resp=response)
 end
-
-
 
 """
     easymirror(v::AbstractVector)
@@ -55,11 +95,10 @@ apropriatly.
 function easymirror end
 export easymirror
 
-
-function easymirror(input::AbstractVector)
-    mirrored = fftshift(vcat(input, reverse(s[begin+1:end])))
-    mirrored ./= 2
+function easymirror(s::AbstractVector)
+    mirrored = FFTW.fftshift(vcat(s ./ 2, reverse(s[begin+1:end] ./ 2)))
     mirrored[end÷2+1] *= 2
+    return mirrored
 end
 
 function easymirror(input::NamedTuple)
