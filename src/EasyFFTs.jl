4b1dcd76bda4b32ca0e26888914520e7e50c6519
KronosTheLate <densbal@live.no>
2022-10-03 13:37:48 +0200

    Fix easymirror

--- src/EasyFFTs.jl
+++ src/EasyFFTs.jl
@@ -21,6 +21,9 @@ If a sampling frequency `fs` is supplied, the output becomes
 a NamedTuple with keys `freq` and `resp`, containing the 
 freqiencues and response respectivly.
 
+The optional function `f` allows the user to pass `abs` or `angle` 
+to get only the amplitude or phase of the response directly.
+
 This function offers four main benefits to using the FFTW functions directly:
 - The output is scaled by default, making the absolute value of the response 
 correspond directly to the amplitude of the sinusoids that make up the signal.
@@ -30,6 +33,28 @@ correspond directly to the amplitude of the sinusoids that make up the signal.
 the common newbie mistake of always using `fft`. Benefits are faster computation 
 and automtically discarding half of the symmetric spectrum. If you want both 
 sides of the spectrum, see [`easymirror`](@ref).
+
+# Examples
+```jldoctest
+julia> using EasyFFTs
+
+julia> s = sin.(1:5);
+
+julia> easyfft(s)
+3-element Vector{ComplexF64}:
+  0.0587205499074596 + 0.0im
+   0.441411013590527 - 0.76819000942203im
+ 0.23045453212899036 - 0.08137937206396029im
+
+julia> easyfft(s, f=abs)
+3-element Vector{Float64}:
+ 0.0587205499074596
+ 0.8859794430430284
+ 0.24440109160213735
+
+julia> easyfft(s, 1, f=abs)
+(freq = [0.0, 0.2, 0.4], resp = [0.0587205499074596, 0.8859794430430284, 0.24440109160213735])
+```
 """
 function easyfft end
 export easyfft
@@ -96,6 +121,29 @@ Given a one-sided spectrum, return a two-sided version
 by "mirroring" about 0. This convenience function also 
 ajusts the amplitude of `v`, or the amplitudes of `s.resp`
 apropriatly.
+
+# Examples
+```jldoctest
+julia> easymirror(0:3)   # Mirroring the amplitudes
+7-element Vector{Float64}:
+ 1.5
+ 1.0
+ 0.5
+ 0.0
+ 0.5
+ 1.0
+ 1.5
+
+julia> easymirror(fill(1, 4))   # Not halving the zero frequency component
+7-element Vector{Float64}:
+ 0.5
+ 0.5
+ 0.5
+ 1.0
+ 0.5
+ 0.5
+ 0.5
+```
 """
 function easymirror end
 export easymirror
@@ -107,7 +155,7 @@ function easymirror(s::AbstractVector)
 end
 
 function easymirror(input::NamedTuple)
-    has_expected_keys = haskey(nt, :freq) && haskey(nt, :resp)
+    has_expected_keys = haskey(input, :freq) && haskey(input, :resp)
     if !has_expected_keys
         error("""
             Expected input to have keys `freq` and `resp`.
@@ -115,16 +163,7 @@ function easymirror(input::NamedTuple)
         """)
     end
 
-    # If all freqiencies are non-negative, mirroring is appropriate.
-    # If else, I am not sure.
-    if input.resp.n != input.resp.n_nonnegative
-        @warn "Based on the `freq` field of the input, it does not look 
-        like it should be mirrored. Proceed at your own risk."
-    end
-    N = length(input.freq)
-
-    # Constructing frequencies manually to avoid allocations
-    freq = FFTW.Frequencies(N, N - 1, input.freq.multiplier)
+    freq = FFTW.fftshift(vcat(input.freq, reverse(input.freq[begin+1:end]) .* 1))
     resp = easymirror(input.resp)
 
     return (; freq, resp)
