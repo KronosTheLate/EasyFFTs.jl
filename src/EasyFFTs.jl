e1eda99d87cc0d2b2760772a68e29f3155d81160
KronosTheLate <densbal@live.no>
2022-10-03 12:00:18 +0200

    abstractArray -> vector

--- src/EasyFFTs.jl
+++ src/EasyFFTs.jl
@@ -8,17 +8,15 @@ Doctstring goes here
 """
 function easyfft end
 export easyfft
-#! FFTSHIFT if doublesided
-#! define easymirror, remember to halv response.
-#! define for abstractarray, Frequencies and named tuple with keys "freq" and "resp".
-function easyfft(s::AbstractArray; scalebylength=true)
+
+function easyfft(s::AbstractVector; scalebylength=true)
     response = FFTW.fft(s)
     if scalebylength
         response ./= length(response)
     end
     return FFTW.fftshift(response)
 end
-function easyfft(s::AbstractArray, fs::Real; scalebylength=true)
+function easyfft(s::AbstractVector, fs::Real; scalebylength=true)
     response = FFTW.fft(s)
     if scalebylength
         response ./= length(response)
@@ -26,7 +24,7 @@ function easyfft(s::AbstractArray, fs::Real; scalebylength=true)
     return (freqs=FFTW.fftshift(FFTW.fftfreq(length(s), fs)), resp=FFTW.fftshift(response))
 end
 
-function easyfft(s::AbstractArray{<:Real}; scalebylength=true)
+function easyfft(s::AbstractVector{<:Real}; scalebylength=true)
     response = FFTW.rfft(s)
     response = vcat(response[1], response[2:end])
     if scalebylength
@@ -34,7 +32,7 @@ function easyfft(s::AbstractArray{<:Real}; scalebylength=true)
     end
     return response
 end
-function easyfft(s::AbstractArray{<:Real}, fs::Real; scalebylength=true)
+function easyfft(s::AbstractVector{<:Real}, fs::Real; scalebylength=true)
     response = FFTW.rfft(s)
     response = vcat(response[1], response[2:end])
     if scalebylength
@@ -42,5 +40,25 @@ function easyfft(s::AbstractArray{<:Real}, fs::Real; scalebylength=true)
     end
     return (freqs=FFTW.rfftfreq(length(s), fs), resp=response)
 end
+
+
+
+"""
+
+"""
+function easymirror end
+export easymirror
+
+
+function easymirror(input::AbstractVector)
+
+end
+
+function easymirror(input::NamedTuple)
+
+end
+
+#! define easymirror, remember to halv response.
+#! define for AbstractVector, Frequencies and named tuple with keys "freq" and "resp".
 
 end #module
