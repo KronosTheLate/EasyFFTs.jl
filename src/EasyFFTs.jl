5c0b245c6667f4044a9612217d58f80ec3360d9e
KronosTheLate <densbal@live.no>
2022-10-03 09:49:51 +0200

    remove tests in src

--- src/EasyFFTs.jl
+++ src/EasyFFTs.jl
@@ -44,36 +44,3 @@ function easyfft(s::AbstractArray{<:Real}, fs::Real; scalebylength=true)
 end
 
 end #module
-
-using EasyFFTs
-
-easyfft(sin.(1:10))
-easyfft(sin.(1:10), 1)
-
-##? Internal testing
-using WGLMakie
-##! Make easyfft function
-##! Make _easyfft_withfreq,
-##! and _easyfft_withoutfreq. Or internal helper functions?
-##! kwarg f, default to identity, applies to response.
-let  #! Why is amp off by faactor of 2?
-    fs = 1000
-    duration = 1
-    ts = range(0, duration, step=1 / fs)
-
-    f = 5
-    A = 2
-    s1 = @. A * sin(f * 2π * ts)
-    s2 = @. 1 * sin(2f * 2π * ts)
-    s = s1 .+ s2
-    fig, ax, _ = stem(ts, s, axis=(title="Signal",))
-
-    s_fft = easyfft(s, fs)
-    stem(fig[2, 1], s_fft.freqs, s_fft.resp .|> abs, axis=(title="FFT of signal",))
-
-    freqs, resp = easyfft(s, fs)
-    stem(fig[3, 1], freqs, resp .|> abs, axis=(title="FFT of signal",))
-
-    fig
-
-end
