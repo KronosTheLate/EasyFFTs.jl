7ef949abfd67ae03bad5a8a263858774d03a0fd7
Orjan Ameye <orjan.ameye@hotmail.com>
2024-10-23 21:02:54 +0200

    add window

--- src/EasyFFTs.jl
+++ src/EasyFFTs.jl
@@ -3,6 +3,7 @@ module EasyFFTs
 
 using Reexport
 @reexport using FFTW
+@reexport using DSP
 
 include("EasyFFT_type.jl")
 include("plotting.jl")
@@ -18,8 +19,8 @@ input vector `s`, scaling by `1/length(s)` by default.
 This function uses FFTW.rfft if `s` has real elements,
 and FFTW.fft otherwise.
 
-Note that if `s` has real elements, the one-side spectrum 
-is returned. This means that the amplitude of the frequencies 
+Note that if `s` has real elements, the one-side spectrum
+is returned. This means that the amplitude of the frequencies
 is doubled, excluding the frequency=0 component. To get the full symmetric spectrum for real signals, use [`easymirror`](@ref), or change the element type of the signal by something like `easyfft(signal.|>ComplexF64)`.
 
 The output is an `EasyFFT` object, with fields `freq` and `resp` containing the frequences and
@@ -40,26 +41,30 @@ julia> s = sin.(2π * 2 * timestamps); # sine of frequency = 2 Hz
 
 julia> easyfft(s, fs)
 EasyFFT with 51 samples.
-Dominant component(s):       
-   Frequency  │  Magnitude   
+Dominant component(s):
+   Frequency  │  Magnitude
 ╺━━━━━━━━━━━━━┿━━━━━━━━━━━━━╸
-    1.9802    │   0.98461    
+    1.9802    │   0.98461
 
 julia> easyfft(s)  # `fs` defaults to 1
 EasyFFT with 51 samples.
-Dominant component(s):       
-   Frequency  │  Magnitude   
+Dominant component(s):
+   Frequency  │  Magnitude
 ╺━━━━━━━━━━━━━┿━━━━━━━━━━━━━╸
-   0.019802   │   0.98461    
-              ╵                      
+   0.019802   │   0.98461
+              ╵
 
 ```
 """
 function easyfft end
 export easyfft
 
-function easyfft(s::AbstractVector, fs::Real=1.0; scalebylength=true)
-    resp = FFTW.fft(s)
+function easyfft(
+    s::AbstractVector, fs::Real=1.0; scalebylength=true, window=DSP.Windows.hanning
+)
+    w = window(length(s))
+
+    resp = length(s)/sum(w)*FFTW.fft(w .* s)
     if scalebylength
         resp ./= length(s)
     end
@@ -69,8 +74,12 @@ function easyfft(s::AbstractVector, fs::Real=1.0; scalebylength=true)
     return EasyFFT(freq, resp)
 end
 
-function easyfft(s::AbstractVector{<:Real}, fs::Real=1.0; scalebylength=true)
-    resp = FFTW.rfft(s)
+function easyfft(
+    s::AbstractVector{<:Real}, fs::Real=1.0; scalebylength=true, window=DSP.Windows.hanning
+)
+    w = window(length(s))
+
+    resp = length(s)/sum(w)*FFTW.rfft(w .* s)
     resp[1] /= 2
     resp .*= 2
     if scalebylength
